2699b3a17d4cb14d130319c53d09e1599a1c292f
Daniel Bolin <axbolin@iu.edu>
2026-05-07 09:57:33 -0400

    chore: tweak settings & configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,16 @@ const baseConfig = [
   {
     files: ['**/package.json'],
     rules: {
-      '@nx/dependency-checks': 'error',
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: [
+            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
+            '{projectRoot}/vite.config.{js,ts,mjs,mts}',
+            '{projectRoot}/vitest.config.{js,ts,mjs,mts}',
+          ],
+        },
+      ],
     },
   },
   {
