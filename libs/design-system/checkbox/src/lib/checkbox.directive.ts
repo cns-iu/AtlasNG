2009eb789fd504409e60a2e37b9da670e765cb72
edlu77 <eddd.lu@gmail.com>
2026-05-13 19:51:12 -0400

    docs(design-system): checkbox directive tweaks

--- libs/design-system/checkbox/src/lib/checkbox.directive.ts
+++ libs/design-system/checkbox/src/lib/checkbox.directive.ts
@@ -2,14 +2,12 @@ import { Directive, inject } from '@angular/core';
 import { StyleLoader } from 'libs/cdk/src/lib/style-loader';
 import { CheckboxStylesComponent } from './checkbox-styles/checkbox-styles';
 
-/** Type of Checkbox size */
-export type CheckboxSize = 'small' | 'medium';
-
 /** Directive for Checkbox */
 @Directive({
   selector: 'mat-checkbox',
 })
 export class CheckboxDirective {
+  /** Injects the style loader */
   private styleLoader = inject(StyleLoader);
 
   /** Registers the styles and sets class names for the checkbox */
