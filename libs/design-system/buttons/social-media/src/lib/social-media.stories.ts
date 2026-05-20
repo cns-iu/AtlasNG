f4f0ae6949c90bbe9bcf1a94b300e80ff87fa29b
Daniel Bolin <axbolin@iu.edu>
2026-05-20 12:02:43 -0400

    refactor(design-system): add a color control to social media button stories

--- libs/design-system/buttons/social-media/src/lib/social-media.stories.ts
+++ libs/design-system/buttons/social-media/src/lib/social-media.stories.ts
@@ -1,9 +1,13 @@
-import { Meta, StoryObj } from '@storybook/angular';
+import { argsToTemplate, Meta, StoryObj } from '@storybook/angular';
 import { DEFAULT_SOCIAL_MEDIA_BUTTON_DEFS, SocialMediaButton } from './social-media';
+
+interface CustomizationControls {
+  color?: string;
+}
 
 const IDS = DEFAULT_SOCIAL_MEDIA_BUTTON_DEFS.map((def) => def.id);
 
-const meta: Meta<SocialMediaButton> = {
+const meta: Meta<SocialMediaButton & CustomizationControls> = {
   component: SocialMediaButton,
   title: 'Design System/Buttons/Social Media Button',
   parameters: {
@@ -14,16 +18,25 @@ const meta: Meta<SocialMediaButton> = {
   },
   args: {
     id: IDS[0],
+    color: '#1C1B1E',
   },
   argTypes: {
     id: {
       control: 'select',
       options: IDS,
     },
+    color: {
+      control: 'color',
+      description: 'Custom color for the icon.',
+    },
   },
+  render: (args) => ({
+    props: args,
+    template: `<ang-social-media-button ${argsToTemplate(args)} style="--mat-icon-button-icon-color: ${args.color}" />`,
+  }),
 };
 
 export default meta;
-type Story = StoryObj<SocialMediaButton>;
+type Story = StoryObj<SocialMediaButton & CustomizationControls>;
 
 export const Default: Story = {};
