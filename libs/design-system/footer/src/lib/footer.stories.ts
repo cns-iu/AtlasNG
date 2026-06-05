1d2a65e459c30d4014c84f45af19699b93902de2
edlu77 <eddd.lu@gmail.com>
2026-06-05 19:20:25 -0400

    feat(design-system): add footer

--- libs/design-system/footer/src/lib/footer.stories.ts
+++ libs/design-system/footer/src/lib/footer.stories.ts
@@ -4,6 +4,12 @@ import { Footer } from './footer';
 const meta: Meta = {
   title: 'Design System/Footer',
   component: Footer,
+  args: {
+    logoUrl: 'assets/wpp.svg',
+    socials: ['linkedin', 'youtube', 'instagram', 'facebook', 'github', 'bluesky', 'x'],
+    orgName: 'Whole Person Physiome',
+    orgLink: 'https://www.cns.edu',
+  },
 };
 
 export default meta;
--- libs/design-system/footer/src/lib/footer.ts
+++ libs/design-system/footer/src/lib/footer.ts
@@ -1,10 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AnyLinkCommand } from '@atlasng/common';
+import { SocialMediaButton } from '@atlasng/design-system/buttons/social-media';
+import { TextLink } from '@atlasng/design-system/text-link';
 
 @Component({
   selector: 'ang-footer',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, SocialMediaButton, TextLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Footer {}
+export class Footer {
+  readonly socials = input<string[]>();
+  readonly logoUrl = input<string>();
+  readonly orgName = input<string>();
+  readonly orgLink = input<AnyLinkCommand>();
+
+  readonly fullYear = new Date().getFullYear();
+}
