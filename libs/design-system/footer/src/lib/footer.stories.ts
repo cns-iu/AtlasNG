7f263f685a6a6699b506e7f234a757eb700ed7e4
edlu77 <eddd.lu@gmail.com>
2026-06-05 12:57:42 -0400

    feat(design-system): create footer component

---
+++ libs/design-system/footer/src/lib/footer.stories.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { Footer } from './footer';
+
+const meta: Meta = {
+  title: 'Design System/Footer',
+  component: Footer,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
