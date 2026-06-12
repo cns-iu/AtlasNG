c3925b2ceccae4f98e4ea0e6a1817c6ae0a1eb27
Daniel Bolin <axbolin@iu.edu>
2026-06-12 14:37:37 -0400

    chore: add additional scopes to commitlint

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,16 @@
+import nxScopes from '@commitlint/config-nx-scopes';
+
+const ADDITIONAL_SCOPES = ['release'];
+
 /** @type {import('@commitlint/types').UserConfig} */
 const config = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  rules: {
+    'scope-enum': async (ctx) => {
+      const scopes = await nxScopes.utils.getProjects(ctx);
+      return [2, 'always', [...scopes, ...ADDITIONAL_SCOPES]];
+    },
+  },
 };
 
 export default config;
