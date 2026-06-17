5a54f62392a3ab82d7bbd477b3e3c05574d1dbd1
edlu77 <eddd.lu@gmail.com>
2026-06-17 12:27:22 -0400

    fix(labs): fix header anchor link

--- libs/labs/section-header/src/lib/section-header.ts
+++ libs/labs/section-header/src/lib/section-header.ts
@@ -2,14 +2,14 @@ import { booleanAttribute, ChangeDetectionStrategy, Component, inject, input } f
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { AnyLink, AnyLinkCommand, IdGenerator } from '@atlasng/common';
+import { AnyLinkCommand, IdGenerator } from '@atlasng/common';
 
 @Component({
   selector:
     // eslint-disable-next-line @angular-eslint/component-selector
     `h1[angSectionHeader], h2[angSectionHeader], h3[angSectionHeader],
     h4[angSectionHeader], h5[angSectionHeader], h6[angSectionHeader]`,
-  imports: [MatDividerModule, MatIconModule, MatButtonModule, AnyLink],
+  imports: [MatDividerModule, MatIconModule, MatButtonModule],
   templateUrl: './section-header.html',
   styleUrl: './section-header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
