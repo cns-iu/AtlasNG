74f3645a714fcbcc91e8841176548fb7bcf3a3fa
edlu77 <eddd.lu@gmail.com>
2026-06-17 17:55:13 -0400

    feat(labs): add table

--- libs/labs/table/src/lib/table.ts
+++ libs/labs/table/src/lib/table.ts
@@ -1,10 +1,64 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, input, viewChild } from '@angular/core';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { TextLink } from '@atlasng/design-system/text-link';
+
+export type TableVariant = 'alternating' | 'divider' | 'basic';
+
+export type TableRow = Record<string, string | number | TableCell>;
+
+export type TableCell = {
+  label: string | number;
+  link?: string;
+};
+
+export interface TableColumn {
+  column: string;
+  label: string;
+  align?: 'left' | 'center' | 'right';
+}
 
 @Component({
   selector: 'ang-table',
-  imports: [],
+  imports: [MatTableModule, MatSortModule, TextLink],
   templateUrl: './table.html',
   styleUrl: './table.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class]': '"ang-table-variant-" + variant()',
+  },
 })
-export class Table {}
+export class Table<T = TableRow> {
+  /** Table data source */
+  protected readonly dataSource = new MatTableDataSource<T>([]);
+
+  /** Table variant */
+  readonly variant = input<TableVariant>('alternating');
+
+  /** Enables sorting */
+  readonly enableSort = input<boolean>(false);
+
+  /** Table data rows */
+  readonly rows = input<T[]>([]);
+
+  /** Columns in table */
+  readonly columns = input<TableColumn[]>([]);
+
+  readonly stickyHeader = input<boolean>(true);
+
+  readonly columnIds = computed(() => this.columns().map((col) => col.column));
+
+  /** Mat sort element */
+  private readonly sort = viewChild.required(MatSort);
+
+  /** Sort data on load and set columns */
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.rows();
+    });
+
+    effect(() => {
+      this.dataSource.sort = this.sort();
+    });
+  }
+}
