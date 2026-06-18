a67394e1e010419228f5d2abdb21d4a7678aacb1
edlu77 <eddd.lu@gmail.com>
2026-06-18 10:45:42 -0400

    chore(labs): update index.ts

--- libs/labs/header-shell/src/index.ts
+++ libs/labs/header-shell/src/index.ts
@@ -1,2 +1,4 @@
 export { HeaderShell } from './lib/header-shell';
+export { NavigationMenu } from './lib/navigation-menu/navigation-menu';
+export { NavigationContainer } from './lib/navigation-container/navigation-container';
 export type { HeaderShellNavigationItem } from './lib/header-shell';
