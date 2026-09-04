02c697a4db3f6e2d16251a05b6441bb74dbb64c5
Daniel Bolin <axbolin@iu.edu>
2026-09-04 12:48:26 -0400

    refactor(design-system): add table entry point

---
+++ libs/design-system/table/src/lib/table.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ang-table',
+  imports: [],
+  templateUrl: './table.html',
+  styleUrl: './table.scss',
+  host: { class: 'ang-table' },
+})
+export class Table {}
