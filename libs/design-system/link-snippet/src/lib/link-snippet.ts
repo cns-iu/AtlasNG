23c3d6ce9260fcac58a026a1481a5ceb7943862f
edlu77 <eddd.lu@gmail.com>
2026-09-16 14:11:51 -0400

    fix(design-system): tweaks

--- libs/design-system/link-snippet/src/lib/link-snippet.ts
+++ libs/design-system/link-snippet/src/lib/link-snippet.ts
@@ -1,12 +1,16 @@
-import { Component, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, computed, inject, input, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AnyLink, IdGenerator } from '@atlasng/common';
+
+/** Regular expression to match URL prefixes. */
+const URL_PREFIX_REGEX = /^(https?:\/\/)?(www\.)?/i;
 
 /** Displays a link in a code-snippet-style presentation with related actions. */
 @Component({
   selector: 'ang-link-snippet',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [AnyLink, MatIconModule, MatButtonModule],
   templateUrl: './link-snippet.html',
   styleUrl: './link-snippet.scss',
   encapsulation: ViewEncapsulation.None,
@@ -18,17 +22,23 @@ export class LinkSnippet {
   /** Url to display */
   readonly url = input.required<string>();
 
+  /** Label for the url with the URL prefix removed */
+  protected readonly urlLabel = computed(() => this.url().replace(URL_PREFIX_REGEX, ''));
+
+  /** ID for the url label */
+  protected readonly labelId = inject(IdGenerator).getId('ang-link-snippet--label');
+
   /** Snackbar service */
-  private readonly snackbar = inject(MatSnackBar);
+  readonly #snackbar = inject(MatSnackBar);
 
   /**
    * Copys url to clipboard and shows a snackbar notification.
    */
   copyUrl(): void {
     navigator.clipboard.writeText(this.url());
-    this.snackbar.open('Link copied', '', {
+    this.#snackbar.open('Link copied', '', {
       duration: 2000,
-      panelClass: 'copy-snackbar',
+      panelClass: 'ang-link-snippet--copy-notification-panel',
       verticalPosition: 'top',
       politeness: 'polite',
     });
