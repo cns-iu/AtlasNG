627c145983d86c81a295cdcd03fa4336202ab422
Daniel Bolin <axbolin@iu.edu>
2026-09-21 16:17:15 -0400

    refactor: :truck: move common storybook configuration to an internal library

--- libs/applications/kg-explorer/.storybook/main.ts
+++ libs/applications/kg-explorer/.storybook/main.ts
@@ -1,22 +1,5 @@
-import type { StorybookConfig } from '@storybook/angular';
+import { createStorybookMainConfig } from '../../../internal/storybook/src/index.ts';
 
-const config: StorybookConfig = {
-  stories: ['../**/*.mdx', '../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-designs', '@storybook/addon-docs', '@storybook/addon-themes'],
-  framework: {
-    name: '@storybook/angular',
-    options: {},
-  },
-  core: {
-    disableTelemetry: true,
-  },
-  docs: {
-    defaultName: 'Documentation',
-  },
-};
+const config = createStorybookMainConfig();
 
 export default config;
-
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/recipes/storybook/custom-builder-configs
