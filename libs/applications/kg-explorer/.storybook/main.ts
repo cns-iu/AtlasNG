c270014ad9459f2207fb60c107b18690d29e1cc4
Daniel Bolin <axbolin@iu.edu>
2026-09-21 13:34:03 -0400

    feat(kg-explorer): create kg-explorer library

---
+++ libs/applications/kg-explorer/.storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../**/*.mdx', '../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-designs', '@storybook/addon-docs', '@storybook/addon-themes'],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  docs: {
+    defaultName: 'Documentation',
+  },
+};
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
