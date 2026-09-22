6cfb2a09cd6bdf0ead97e4d8fb3d1724661eb5af
Daniel Bolin <axbolin@iu.edu>
2026-09-22 14:05:45 -0400

    feat(kg-explorer): setup publishable library (#116)
    * feat(kg-explorer): create kg-explorer library
    
    * refactor: :truck: move common storybook configuration to an internal library
    
    * refactor: :wrench: use storybook plugin to infer tasks
    
    * refactor(labs): update labs breakpoint sass to mirror design-system
    
    * refactor(labs): :truck: move storybook assets into .stroybook folder
    
    also fix an incorrect asset path in header-shell stories

---
+++ libs/applications/kg-explorer/src/lib/kg-explorer/kg-explorer.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/** Root component for the knowledge graph explorer application. */
+@Component({
+  selector: 'ang-kg-explorer',
+  imports: [],
+  templateUrl: './kg-explorer.html',
+  styleUrl: './kg-explorer.scss',
+  host: {
+    class: 'ang-kg-explorer',
+  },
+})
+export class KgExplorer {}
