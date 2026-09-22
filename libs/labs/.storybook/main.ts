7c5c3f75f7d13ba92b484c44055be810ff6d6a32
Daniel Bolin <axbolin@iu.edu>
2026-09-22 11:56:25 -0400

    refactor(labs): :truck: move storybook assets into .stroybook folder
    also fix an incorrect asset path in header-shell stories

--- libs/labs/.storybook/main.ts
+++ libs/labs/.storybook/main.ts
@@ -1,7 +1,7 @@
 import { createStorybookMainConfig } from '../../internal/storybook/src/index.ts';
 
 const config = createStorybookMainConfig({
-  staticDirs: [{ from: '../assets', to: 'assets' }],
+  staticDirs: [{ from: './assets', to: 'assets' }],
 });
 
 export default config;
