b883d9246ceecdbc04517f9ae89de0de215a1fa4
Edward Lu <36934022+edlu77@users.noreply.github.com>
2026-09-22 17:32:19 -0400

    feat(design-system): add error pages (#112)
    * feat(design-system): add error pages
    
    * fix(design-system): error page tweaks
    
    * fix(design-system): remove unused @angular/router peer dependency
    
    Co-authored-by: edlu77 <edlu77@users.noreply.github.com>
    
    * refactor(design-system): create base error page and make all parts dynamic
    
    * refactor(design-system): more improvements
    
    * refactor(design-system): error page tweaks
    
    ---------
    
    Co-authored-by: nx-cloud[bot] <71083854+nx-cloud[bot]@users.noreply.github.com>
    Co-authored-by: edlu77 <edlu77@users.noreply.github.com>

---
+++ libs/design-system/error-page/src/index.ts
@@ -0,0 +1,3 @@
+export { ErrorPage, ErrorPageAction, ErrorPageDescription, ErrorPageTitle } from './lib/error-page';
+export { NotFoundPage } from './lib/not-found-page/not-found-page';
+export { ServerErrorPage } from './lib/server-error-page/server-error-page';
